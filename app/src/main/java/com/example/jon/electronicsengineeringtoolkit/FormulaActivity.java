3a82a894037739436da046570a0d8212906b5ab9
willywil <jonwilhelmjr@hotmail.com>
2018-06-25 18:40:53 -0500

    This is to mark the working main menu buttons opening respective activities.

--- app/src/main/java/com/example/jon/electronicsengineeringtoolkit/FormulaActivity.java
+++ app/src/main/java/com/example/jon/electronicsengineeringtoolkit/FormulaActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class FormulaActivity extends Activity {
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formula);
--- app/src/main/java/com/example/jon/electronicsengineeringtoolkit/OhmActivity.java
+++ app/src/main/java/com/example/jon/electronicsengineeringtoolkit/OhmActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class OhmActivity extends Activity {
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ohm);
